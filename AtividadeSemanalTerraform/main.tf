c7da564ee6937f4ee8a9b352fd0cdb6d24464a41
Josimar Henrique de Oliveira <josimarhenriquedeoliveira@gmail.com>
2025-06-03 20:53:59 -0300

    teste

--- AtividadeSemanalTerraform/main.tf
+++ AtividadeSemanalTerraform/main.tf
@@ -46,3 +46,8 @@ resource "google_compute_instance" "vm_instance" {
     sudo apt install -y python3
   EOT
 }
+
+output "vm_instance_ip" {
+  description = "O endereço IP externo da VM."
+  value       = google_compute_instance.default.network_interface[0].access_config[0].nat_ip
+}
