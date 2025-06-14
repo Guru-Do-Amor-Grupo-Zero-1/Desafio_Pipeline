d128379e84fd856c5e67a16c16c293efc4f8edb0
regi <reginaldosgomesj@gmail.com>
2025-06-14 16:54:25 -0300

    Alterado arquivos para fazer deploy de um GKE

--- AtividadeSemanalTerraform/main.tf
+++ AtividadeSemanalTerraform/main.tf
@@ -4,84 +4,53 @@ provider "google" {
   zone    = var.zone
 }
 
-variable "ssh_pub_key" {
-  description = "A chave pública SSH para o usuário de automação."
-  type        = string
+# Criação de rede para o cluster
+resource "google_compute_network" "k8s_network" {
+  name = "gke-network"
 }
 
-resource "google_compute_network" "default" {
-  name = "network-iac"
-}
-
-resource "google_compute_firewall" "default" {
-  name    = "allow-ssh-http"
-  network = google_compute_network.default.name
+# Firewall para permitir acesso ao cluster e apps (inclui HTTP/Grafana/etc.)
+resource "google_compute_firewall" "gke_firewall" {
+  name    = "gke-firewall"
+  network = google_compute_network.k8s_network.name
 
   allow {
     protocol = "tcp"
-    ports    = ["22", "80", "443", "9090","3000", "5000"]
+    ports    = ["22", "80", "443", "3000", "9090", "5000"]
   }
 
   source_ranges = ["0.0.0.0/0"]
 }
 
-resource "google_compute_instance" "vm_instance" {
-  name         = var.vm_name
-  machine_type = var.machine_type
-  zone         = var.zone
+# Cluster GKE
+resource "google_container_cluster" "primary" {
+  name     = var.cluster_name
+  location = var.region
 
-  boot_disk {
-    initialize_params {
-      image = "${var.image_project}/${var.image_family}"
-      size  = var.disk_size
-    }
-  }
+  remove_default_node_pool = true
+  initial_node_count       = 1
 
-  network_interface {
-    network = google_compute_network.default.name
-    access_config {
-      // Isso gera um IP público
-    }
-  }
-
-  // Script de inicialização robusto para criar o usuário e configurar o SSH
-  metadata_startup_script = <<-EOT
-    #!/bin/bash
-    # Espera um pouco para garantir que a rede esteja pronta
-    sleep 10
-
-    # Cria o usuário 'josim' se ele não existir
-    id -u josim &>/dev/null || useradd -m -s /bin/bash -G google-sudoers josim
-
-    # Define o diretório home e o arquivo de chaves autorizadas
-    USER_HOME="/home/josim"
-    SSH_DIR="$USER_HOME/.ssh"
-    AUTH_KEYS_FILE="$SSH_DIR/authorized_keys"
-
-    # Cria o diretório .ssh e o arquivo authorized_keys
-    mkdir -p "$SSH_DIR"
-    touch "$AUTH_KEYS_FILE"
-
-    # Adiciona a chave pública ao arquivo de chaves autorizadas
-    echo "${var.ssh_pub_key}" >> "$AUTH_KEYS_FILE"
-
-    # Define as permissões corretas
-    chown -R josim:josim "$USER_HOME"
-    chmod 700 "$SSH_DIR"
-    chmod 600 "$AUTH_KEYS_FILE"
-
-    # Instala dependências básicas
-    apt-get update
-    apt-get install -y python3
-  EOT
-
-  // Remove a dependência dos metadados de SSH do projeto para evitar conflitos
-  metadata = {
-    block-project-ssh-keys = "TRUE"
-  }
+  network    = google_compute_network.k8s_network.name
+  subnetwork = null
 }
 
-output "vm_instance_ip" {
-  description = "O endereço IP externo da VM."
-  value       = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
+# Node Pool
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "primary-node-pool"
+  location   = var.region
+  cluster    = google_container_cluster.primary.name
+
+  node_config {
+    machine_type = "e2-medium"
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+  }
+
+  node_count = 1
+}
+
+# Output para IP de acesso (se quiser usar mais tarde)
+output "cluster_endpoint" {
+  value = google_container_cluster.primary.endpoint
 }
--- AtividadeSemanalTerraform/output.tf
+++ AtividadeSemanalTerraform/output.tf
@@ -1,3 +1,4 @@
-output "instance_ip" {
-  value = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
+output "cluster_endpoint" {
+  description = "Endpoint do cluster Kubernetes GKE"
+  value       = google_container_cluster.primary.endpoint
 }
