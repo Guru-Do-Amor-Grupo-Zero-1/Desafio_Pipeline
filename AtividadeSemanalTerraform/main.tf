f4026a9a0fce7efe8da13dd84c6a6334836d2cea
reginaldo <reginaldosgomesj@gmail.com>
2025-06-14 17:31:54 -0300

    Teste

--- AtividadeSemanalTerraform/main.tf
+++ AtividadeSemanalTerraform/main.tf
@@ -16,7 +16,7 @@ resource "google_compute_network" "k8s_network" {
 
 # Firewall para permitir acesso ao cluster e apps (inclui HTTP/Grafana/etc.) d
 resource "google_compute_firewall" "gke_firewall" {
-  name    = "gke-firewall"
+  name    = "gke-firewall-v2" # ou importe o existente
   network = google_compute_network.k8s_network.name
 
   allow {
@@ -27,26 +27,16 @@ resource "google_compute_firewall" "gke_firewall" {
   source_ranges = ["0.0.0.0/0"]
 }
 
-# Cluster GKE
-resource "google_container_cluster" "primary" {
-  name     = var.cluster_name
-  location = var.region
-
-  remove_default_node_pool = true
-  initial_node_count       = 1
-
-  network    = google_compute_network.k8s_network.name
-  subnetwork = null
-}
-
-# Node Pool
 resource "google_container_node_pool" "primary_nodes" {
-  name       = "primary-node-pool"
-  location   = var.region
-  cluster    = google_container_cluster.primary.name
+  name     = "primary-node-pool"
+  location = var.region
+  cluster  = google_container_cluster.primary.name
 
   node_config {
     machine_type = "e2-medium"
+    disk_type    = "pd-standard" # <== Essencial para evitar o erro de quota
+    disk_size_gb = 50
+
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform",
     ]
@@ -54,4 +44,3 @@ resource "google_container_node_pool" "primary_nodes" {
 
   node_count = 1
 }
-
