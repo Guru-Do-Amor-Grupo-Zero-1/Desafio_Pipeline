4cf2ff37c74f873a57e5b9fae7ac41761977d92d
Josimar <josimarhenriquedeoliveira@gmail.com>
2025-06-17 15:40:40 -0300

    teste

--- AtividadeSemanalTerraform/variables.tf
+++ AtividadeSemanalTerraform/variables.tf
@@ -1,14 +1,14 @@
 variable "project_id" {
   description = "cluster-mensal-grupo-zero1"
   type = string
-  default = "projeto-mensal-04"
+  default = "projeto-mensal-04-prod"
 }
 
 variable "region" {
   default = "us-central1"
 }
 variable "zone" {
-  default = "us-central1-a"
+  default = "us-central1-f"
 }
 variable "cluster_name" {
   default = "cluster-entrega"
